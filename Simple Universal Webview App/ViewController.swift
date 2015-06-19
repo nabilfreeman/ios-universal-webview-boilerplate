26f203ec688032e1ca81f4d04054fcedce579154
Nabil Freeman <nabil@matchchat.co.uk>
2015-06-19 14:50:27 +0100

    Initial Commit

---
+++ Simple Universal Webview App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple Universal Webview App
+//
+//  Created by Nabil Freeman on 19/06/2015.
+//  Copyright (c) 2015 Freeman Industries. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
