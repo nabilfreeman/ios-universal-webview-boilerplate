fe86431ba8523c3f2e5ca8e76cadf15693adaf91
Fehim Tabak <fehim.tabak@helloprint.com>
2020-05-19 20:58:39 +0200

    Bare minimum changes to make it work with Swift 5 & Xcode 11.4

--- Simple Universal Webview App/ViewController.swift
+++ Simple Universal Webview App/ViewController.swift
@@ -10,39 +10,39 @@ import UIKit
 import WebKit
 
 class ViewController: UIViewController {
-    
+
     var wkWebView: WKWebView?
     var uiWebView: UIWebView?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
         // Get main screen rect size
-        let screenSize: CGRect = UIScreen.mainScreen().bounds
-        
+        let screenSize: CGRect = UIScreen.main.bounds
+
         // Construct frame where webview will be pop
         let frameRect: CGRect = CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height)
 
         // Create url request from local index.html file located in web_content
-        let url: NSURL = NSBundle.mainBundle().URLForResource("web_content/index", withExtension: "html")!
-        
-        // Create a url request that points to a remote server (uncomment this line to use a remote url)
-        // let url: NSURL = NSURL(string: "http://example.com")!;
+        //let url: NSURL = Bundle.mainBundle().URLForResource("web_content/index", withExtension: "html")!
 
-        let requestObj: NSURLRequest = NSURLRequest(URL: url);
-        
+        // Create a url request that points to a remote server (uncomment this line to use a remote url)
+        let url: NSURL = NSURL(string: "https://example.com")!;
+
+        let requestObj: NSURLRequest = NSURLRequest(url: url as URL);
+
         // Test operating system
-        if NSProcessInfo().isOperatingSystemAtLeastVersion(NSOperatingSystemVersion(majorVersion: 8, minorVersion: 0, patchVersion: 0)) {
-            
+        if ProcessInfo().isOperatingSystemAtLeast(OperatingSystemVersion(majorVersion: 8, minorVersion: 0, patchVersion: 0)) {
+
             self.wkWebView = WKWebView(frame: frameRect)
-            self.wkWebView?.loadRequest(requestObj)
+            self.wkWebView?.load(requestObj as URLRequest)
             self.view.addSubview(self.wkWebView!)
 
         } else {
 
             self.uiWebView = UIWebView(frame: frameRect)
-            self.uiWebView?.loadRequest(requestObj)
+            self.uiWebView?.loadRequest(requestObj as URLRequest)
             self.view.addSubview(self.uiWebView!)
         }
 
@@ -52,11 +52,11 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     //Commented:    black status bar.
     //Uncommented:  white status bar.
-    override func preferredStatusBarStyle() -> UIStatusBarStyle {
-        return UIStatusBarStyle.LightContent
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
 
 }
